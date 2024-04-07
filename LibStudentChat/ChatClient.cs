f24318098dda3bbcf2fd41ae1dcae9de88110e3f
SlimeNull <slimenull@outlook.com>
2024-04-07 15:07:39 +0800

    feat: 实现 ChatClient 事件通知

--- LibStudentChat/ChatClient.cs
+++ LibStudentChat/ChatClient.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics.Tracing;
+using System.Text.Json;
 using LibStudentChat.Models.Events;
+using LibStudentChat.Utilites;
 
 namespace LibStudentChat;
 
@@ -24,10 +26,70 @@ public class ChatClient
 
     private async Task BackgroundTasks(CancellationToken cancellationToken)
     {
+        const string EventNamePrivateMessage = "privateMessage";
+        const string EventNameGroupMessage = "groupMessage";
+        const string EventNameFriendRequest = "friendRequest";
+        const string EventNameGroupRequest = "groupRequest";
+        const string EventNameFriendIncreased = "friendIncreased";
+        const string EventNameFriendDecreased = "friendDecreased";
+        const string EventNameGroupIncreased = "groupIncreased";
+        const string EventNameGroupDecreased = "groupDecreased";
+
         var request = new HttpRequestMessage(HttpMethod.Get, new Uri("/api/Notify", UriKind.Relative));
         var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
-
+        var serverSentEvents = ServerSentEvent.EnumerateFromStream(response.Content.ReadAsStream(), cancellationToken);
         
+        await foreach (var serverSentEvent in serverSentEvents)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (serverSentEvent.Data is null)
+            {
+                continue;
+            }
+
+            try
+            {
+                switch (serverSentEvent.Event)
+                {
+                    case EventNamePrivateMessage:
+                        PrivateMessageReceived?.Invoke(this, new PrivateMessageReceivedEventArgs(JsonSerializer.Deserialize<Models.PrivateMessage>(serverSentEvent.Data)!));
+                        break;
+
+                    case EventNameGroupMessage:
+                        GroupMessageReceived?.Invoke(this, new GroupMessageReceivedEventArgs(JsonSerializer.Deserialize<Models.GroupMessage>(serverSentEvent.Data)!));
+                        break;
+
+                    case EventNameFriendRequest:
+                        FriendRequestReceived?.Invoke(this, new FriendRequestReceivedEventArgs(JsonSerializer.Deserialize<Models.FriendRequest>(serverSentEvent.Data)!));
+                        break;
+
+                    case EventNameGroupRequest:
+                        GroupRequestReceived?.Invoke(this, new GroupRequestReceivedEventArgs(JsonSerializer.Deserialize<Models.GroupRequest>(serverSentEvent.Data)!));
+                        break;
+
+                    case EventNameFriendIncreased:
+                        FriendIncreased?.Invoke(this, new FriendChangedEventArgs(JsonSerializer.Deserialize<Models.User>(serverSentEvent.Data)!));
+                        break;
+
+                    case EventNameFriendDecreased:
+                        FriendDecreased?.Invoke(this, new FriendChangedEventArgs(JsonSerializer.Deserialize<Models.User>(serverSentEvent.Data)!));
+                        break;
+
+                    case EventNameGroupIncreased:
+                        GroupIncreased?.Invoke(this, new GroupChangedEventArgs(JsonSerializer.Deserialize<Models.Group>(serverSentEvent.Data)!));
+                        break;
+
+                    case EventNameGroupDecreased:
+                        GroupDecreased?.Invoke(this, new GroupChangedEventArgs(JsonSerializer.Deserialize<Models.Group>(serverSentEvent.Data)!));
+                        break;
+                }
+            }
+            catch
+            {
+                // TODO: log here
+            }
+        }
     }
 
     
