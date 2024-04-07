bedfa33e37c59644e0454bbaa94f1cf435f7236d
SlimeNull <slimenull@outlook.com>
2024-04-07 14:57:45 +0800

    feat: 优化 SSE 实现

--- LibStudentChat/Utilites/SseStream.cs
+++ LibStudentChat/Utilites/SseStream.cs
@@ -1,55 +1,62 @@
-﻿namespace LibStudentChat.Utilites;
+﻿using System.Runtime.CompilerServices;
 
-internal class SseEnumerator : IAsyncEnumerator<SseEvent>, IDisposable
+namespace LibStudentChat.Utilites;
+
+public record class ServerSentEvent(string Event, string? Data, string? Id, string? Retry)
 {
-    private readonly Stream baseStream;
-    private readonly StreamReader reader;
-
-    public SseEvent? Current { get; set; }
-
-    public SseEnumerator(Stream sseStream)
+    public static async IAsyncEnumerable<ServerSentEvent> EnumerateFromStream(Stream stream, [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        baseStream = sseStream;
-        reader = new StreamReader(baseStream);
-    }
+        const string PrefixEvent = "event: ";
+        const string PrefixData = "data: ";
+        const string PrefixId = "id: ";
+        const string PrefixRetry = "retry: ";
 
-    public void Dispose()
-    {
-        baseStream.Dispose();
-    }
+        StreamReader reader = new(stream);
+        ServerSentEvent current = new(string.Empty, null, null, null);
 
-    public async ValueTask<bool> MoveNextAsync()
-    {
-        const string startEvent = "event: ";
-        const string startData = "data: ";
+        while (true)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
 
-        var line = await reader.ReadLineAsync();
-        if (line is null)
-            return false;
+            var line = await reader.ReadLineAsync(cancellationToken);
+            if (line is null)
+            {
+                yield break;
+            }
 
-        if (!line.StartsWith(startEvent))
-            throw new Exception("意外的异常");
-        var eventName = line[startEvent.Length..];
-
-        line = await reader.ReadLineAsync();
-        if (line is null)
-            throw new Exception("未经结束的异常");
-        if (!line.StartsWith(startData))
-            throw new Exception("意外的异常");
-        var data = line[startData.Length..];
-
-        line = await reader.ReadLineAsync();
-        if (line != string.Empty)
-            throw new Exception("意外的异常");
-
-        Current = new SseEvent(eventName, data);
-        return true;
-    }
-
-    public ValueTask DisposeAsync()
-    {
-        return baseStream.DisposeAsync();
+            if (line.StartsWith(PrefixEvent, StringComparison.OrdinalIgnoreCase))
+            {
+                current = current with
+                {
+                    Event = line.Substring(PrefixEvent.Length)
+                };
+            }
+            else if (line.StartsWith(PrefixData, StringComparison.OrdinalIgnoreCase))
+            {
+                current = current with
+                {
+                    Data = line.Substring(PrefixData.Length)
+                };
+            }
+            else if(line.StartsWith(PrefixId, StringComparison.OrdinalIgnoreCase))
+            {
+                current = current with
+                {
+                    Id = line.Substring(PrefixId.Length)
+                };
+            }
+            else if(line.StartsWith(PrefixRetry, StringComparison.OrdinalIgnoreCase))
+            {
+                current = current with
+                {
+                    Retry = line.Substring(PrefixRetry.Length)
+                };
+            }
+            else if (string.IsNullOrWhiteSpace(line))
+            {
+                yield return current;
+                current = new(string.Empty, null, null, null);
+            }
+        }
     }
 }
-
-internal record SseEvent(string EventName, string Data);
