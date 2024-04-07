946334d6ab3c2600703b47ffefe301df3e0c56ae
fairy <me@ilyfairy.com>
2024-04-07 12:49:47 +0800

    登录窗口写好了

--- WpfStudentChat/Views/Windows/MainWindow.xaml.cs
+++ WpfStudentChat/Views/Windows/MainWindow.xaml.cs
@@ -1,64 +1,81 @@
-﻿using Wpf.Ui;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Wpf.Ui;
 using Wpf.Ui.Appearance;
 using Wpf.Ui.Controls;
 using WpfStudentChat.ViewModels.Windows;
 
-namespace WpfStudentChat.Views.Windows
+namespace WpfStudentChat.Views.Windows;
+
+public partial class MainWindow : INavigationWindow
 {
-    public partial class MainWindow : INavigationWindow
+    private readonly IServiceProvider _serviceProvider;
+
+    public MainWindowViewModel ViewModel { get; }
+
+    public MainWindow(
+        MainWindowViewModel viewModel,
+        IPageService pageService,
+        INavigationService navigationService,
+        IServiceProvider serviceProvider
+    )
     {
-        public MainWindowViewModel ViewModel { get; }
+        ViewModel = viewModel;
+        _serviceProvider = serviceProvider;
+        DataContext = this;
 
-        public MainWindow(
-            MainWindowViewModel viewModel,
-            IPageService pageService,
-            INavigationService navigationService
-        )
+        SystemThemeWatcher.Watch(this);
+
+        InitializeComponent();
+        SetPageService(pageService);
+
+        navigationService.SetNavigationControl(RootNavigation);
+
+        Loaded += MainWindow_Loaded;
+    }
+
+    private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+    {
+        var loginWindow = _serviceProvider.GetRequiredService<LoginWindow>();
+        loginWindow.ShowDialog();
+        if (!loginWindow.ViewModel.IsLogged)
         {
-            ViewModel = viewModel;
-            DataContext = this;
-
-            SystemThemeWatcher.Watch(this);
-
-            InitializeComponent();
-            SetPageService(pageService);
-
-            navigationService.SetNavigationControl(RootNavigation);
+            Close();
+            return;
         }
+    }
 
-        #region INavigationWindow methods
+    #region INavigationWindow methods
 
-        public INavigationView GetNavigation() => RootNavigation;
+    public INavigationView GetNavigation() => RootNavigation;
 
-        public bool Navigate(Type pageType) => RootNavigation.Navigate(pageType);
+    public bool Navigate(Type pageType) => RootNavigation.Navigate(pageType);
 
-        public void SetPageService(IPageService pageService) => RootNavigation.SetPageService(pageService);
+    public void SetPageService(IPageService pageService) => RootNavigation.SetPageService(pageService);
 
-        public void ShowWindow() => Show();
+    public void ShowWindow() => Show();
 
-        public void CloseWindow() => Close();
+    public void CloseWindow() => Close();
 
-        #endregion INavigationWindow methods
+    #endregion INavigationWindow methods
 
-        /// <summary>
-        /// Raises the closed event.
-        /// </summary>
-        protected override void OnClosed(EventArgs e)
-        {
-            base.OnClosed(e);
+    /// <summary>
+    /// Raises the closed event.
+    /// </summary>
+    protected override void OnClosed(EventArgs e)
+    {
+        base.OnClosed(e);
 
-            // Make sure that closing this window will begin the process of closing the application.
-            Application.Current.Shutdown();
-        }
+        // Make sure that closing this window will begin the process of closing the application.
+        Application.Current.Shutdown();
+    }
 
-        INavigationView INavigationWindow.GetNavigation()
-        {
-            throw new NotImplementedException();
-        }
+    INavigationView INavigationWindow.GetNavigation()
+    {
+        throw new NotImplementedException();
+    }
 
-        public void SetServiceProvider(IServiceProvider serviceProvider)
-        {
-            throw new NotImplementedException();
-        }
+    public void SetServiceProvider(IServiceProvider serviceProvider)
+    {
+        throw new NotImplementedException();
     }
 }
