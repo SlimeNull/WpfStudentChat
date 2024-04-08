21a6ad8e8efbd7592e9d9eb454211b8879f2901b
SlimeNull <slimenull@outlook.com>
2024-04-08 09:48:25 +0800

    feat: 实现更多接口, 设置自己资料, 群资料, 创建群, 删除群

--- LibStudentChat/Models/Network/APIModels.cs
+++ LibStudentChat/Models/Network/APIModels.cs
@@ -34,12 +34,22 @@ namespace StudentChat.Models.Network
     public record class GetUserResultData(User User);
     public record class GetGroupRequestData(int GroupId);
     public record class GetGroupResultData(Group Group);
+    public record class SetUserRequestData(User User);
+    public record class SetGroupRequestData(Group Group);
+    public record class SetPasswordRequestData(string PasswordHash);
+    public record class CreateGroupRequestData(Group Group);
+    public record class CreateGroupResultData(Group Group);
+    public record class DeleteGroupRequestData(int GroupId);
 
 
     public record class SearchUserRequestData(string Keyword, int Skip, int Count);
     public record class SearchUserResultData(List<User> Users);
     public record class SearchGroupRequestData(string Keyword, int Skip, int Count);
     public record class SearchGroupResultData(List<Group> Groups);
+
+
+    public record class GetFriendsResultData(List<User> Friends);
+    public record class GetGroupsResultData(List<Group> Groups);
 
 
 
--- StudentChat.Server/Controllers/InfoController.cs
+++ StudentChat.Server/Controllers/InfoController.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using StudentChat.Models.Network;
 using StudentChat.Server.Extensions;
 using StudentChat.Server.Models.Database;
+using StudentChat.Server.Services;
 using CommonModels = StudentChat.Models;
 
 namespace StudentChat.Server.Controllers
@@ -16,11 +17,14 @@ namespace StudentChat.Server.Controllers
     public class InfoController : ControllerBase
     {
         private readonly ChatServerDbContext _dbContext;
+        private readonly NotifyService _notifyService;
 
         public InfoController(
-            ChatServerDbContext dbContext)
+            ChatServerDbContext dbContext,
+            NotifyService notifyService)
         {
             _dbContext = dbContext;
+            _notifyService = notifyService;
         }
 
         [HttpPost("GetSelf")]
@@ -31,6 +35,55 @@ namespace StudentChat.Server.Controllers
             return GetUserAsync(new GetUserRequestData(selfId));
         }
 
+        [HttpPost("SetSelf")]
+        public async Task<ApiResult> SetSelfAsync(SetUserRequestData request)
+        {
+            var selfUserId = HttpContext.GetUserId();
+            var user = await _dbContext.Users.FirstAsync(user => user.Id == selfUserId);
+            var newProfile = request.User;
+
+            if (!string.IsNullOrWhiteSpace(newProfile.AvatarHash))
+            {
+                user.AvatarHash = newProfile.AvatarHash;
+            }
+
+            if (!string.IsNullOrWhiteSpace(newProfile.UserName))
+            {
+                user.UserName = newProfile.UserName;
+            }
+
+            if (!string.IsNullOrWhiteSpace(newProfile.Nickname))
+            {
+                user.Nickname = newProfile.Nickname;
+            }
+
+            if (!string.IsNullOrWhiteSpace(newProfile.Bio))
+            {
+                user.Bio = newProfile.Bio;
+            }
+
+            await _dbContext.SaveChangesAsync();
+
+            return ApiResult.CreateOk();
+        }
+
+        [HttpPost("SetSelfPassword")]
+        public async Task<ApiResult> SetSelfPasswordAsync(SetPasswordRequestData request)
+        {
+            var selfUserId = HttpContext.GetUserId();
+            var user = await _dbContext.Users.FirstAsync(user => user.Id == selfUserId);
+
+            if (string.IsNullOrWhiteSpace(request.PasswordHash))
+            {
+                return ApiResult.CreateErr("Invalid password hash");
+            }
+
+            user.PasswordHash = request.PasswordHash;
+
+            await _dbContext.SaveChangesAsync();
+
+            return ApiResult.CreateOk();
+        }
 
         [HttpPost("GetUser")]
         public async Task<ApiResult<GetUserResultData>> GetUserAsync(GetUserRequestData request)
@@ -58,9 +111,107 @@ namespace StudentChat.Server.Controllers
             return ApiResult<GetGroupResultData>.CreateOk(new GetGroupResultData((CommonModels.Group)group));
         }
 
+        [HttpPost("SetGroup")]
+        public async Task<ApiResult> SetGroupAsync(SetGroupRequestData request)
+        {
+            var selfUserId = HttpContext.GetUserId();
+            var group = await _dbContext.Groups.FirstOrDefaultAsync(group => group.Id == request.Group.Id);
+
+            if (group is null)
+            {
+                return ApiResult.CreateErr("Group not found");
+            }
+
+            if (group.OwnerId != selfUserId)
+            {
+                return ApiResult.CreateErr("You are not owner of that group");
+            }
+
+            var newProfile = request.Group;
+
+            if (string.IsNullOrWhiteSpace(newProfile.Name))
+            {
+                group.Name = newProfile.Name;
+            }
+
+            if (string.IsNullOrWhiteSpace(newProfile.Description))
+            {
+                group.Description = newProfile.Description;
+            }
+
+            if (string.IsNullOrWhiteSpace(newProfile.AvatarHash))
+            {
+                group.AvatarHash = newProfile.AvatarHash;
+            }
+
+            await _dbContext.SaveChangesAsync();
+
+            return ApiResult.CreateOk();
+        }
+
+        [HttpPost("CreateGroup")]
+        public async Task<ApiResult<CreateGroupResultData>> CreateGroupAsync(CreateGroupRequestData request)
+        {
+            var selfUserId = HttpContext.GetUserId();
+            var entry = await _dbContext.Groups.AddAsync(
+                new Group()
+                {
+                    Name = request.Group.Name,
+                    Description = request.Group.Description,
+                    AvatarHash = request.Group.AvatarHash,
+                });
+
+            await _dbContext.SaveChangesAsync();
+
+            var commonGroup = (CommonModels.Group)entry.Entity;
+            await _notifyService.OnGroupIncreased(selfUserId, commonGroup);
+
+            return ApiResult<CreateGroupResultData>.CreateOk(new CreateGroupResultData(commonGroup));
+        }
+
+        [HttpPost("DeleteGroup")]
+        public async Task<ApiResult> DeleteGroupAsync(DeleteGroupRequestData request)
+        {
+            var selfUserId = HttpContext.GetUserId();
+            var group = await _dbContext.Groups
+                .Include(group => group.Members)
+                .FirstOrDefaultAsync(group => group.Id == request.GroupId);
+
+            if (group is null)
+            {
+                return ApiResult.CreateErr("Group not found");
+            }
+
+            if (group.OwnerId != selfUserId)
+            {
+                return ApiResult.CreateErr("You are not owner of that group");
+            }
+
+            _dbContext.Groups.Remove(group);
+            await _dbContext.SaveChangesAsync();
+
+            var commonGroup = (CommonModels.Group)group;
+            await _notifyService.OnGroupDecreased(selfUserId, commonGroup);
+
+            foreach (var member in group.Members)
+            {
+                await _notifyService.OnGroupDecreased(member.Id, commonGroup);
+            }
+
+            return ApiResult.CreateOk();
+        }
+
         [HttpPost("SearchUser")]
         public async Task<ApiResult<SearchUserResultData>> SearchUser(SearchUserRequestData request)
         {
+            if (request.Count == 0)
+            {
+                request = request with
+                {
+                    Count = 10
+                };
+            }
+
             var users = await _dbContext.Users
                 .Where(user => user.UserName.Contains(request.Keyword) || user.Nickname.Contains(request.Keyword))
                 .Skip(request.Skip)
@@ -74,6 +225,14 @@ namespace StudentChat.Server.Controllers
         [HttpPost("SearchGroup")]
         public async Task<ApiResult<SearchGroupResultData>> SearchGroup(SearchGroupRequestData request)
         {
+            if (request.Count == 0)
+            {
+                request = request with
+                {
+                    Count = 10
+                };
+            }
+
             var groups = await _dbContext.Groups
                 .Where(group => group.Name.Contains(request.Keyword))
                 .Skip(request.Skip)
@@ -82,6 +241,40 @@ namespace StudentChat.Server.Controllers
                 .ToListAsync();
 
             return ApiResult<SearchGroupResultData>.CreateOk(new SearchGroupResultData(groups));
+        }
+
+        [HttpPost("GetFriends")]
+        public async Task<ApiResult<GetFriendsResultData>> GetFriends()
+        {
+            var selfUserId = HttpContext.GetUserId();
+            var selfUser = await _dbContext.Users
+                .Include(user => user.AddedFriends)
+                .Include(user => user.AcceptedFriends)
+                .FirstAsync(user => user.Id == selfUserId);
+
+            var friends = selfUser.AddedFriends
+                .Concat(selfUser.AcceptedFriends)
+                .Select(user => (CommonModels.User)user)
+                .ToList();
+
+            return ApiResult<GetFriendsResultData>.CreateOk(new GetFriendsResultData(friends));
+        }
+
+        [HttpPost("GetGroups")]
+        public async Task<ApiResult<GetGroupsResultData>> GetGroups()
+        {
+            var selfUserId = HttpContext.GetUserId();
+            var selfUser = await _dbContext.Users
+                .Include(user => user.OwnedGroups)
+                .Include(user => user.JoindGroups)
+                .FirstAsync(user => user.Id == selfUserId);
+
+            var groups = selfUser.OwnedGroups
+                .Concat(selfUser.JoindGroups)
+                .Select(group => (CommonModels.Group)group)
+                .ToList();
+
+            return ApiResult<GetGroupsResultData>.CreateOk(new GetGroupsResultData(groups));
         }
     }
 }
